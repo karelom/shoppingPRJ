ccaba81f456d932eae223b1ee42b2cc31e083e7f
HAN Wenting <wenting.han@efrei.net>
2019-04-02 09:41:29 +0200

    waiting for API

--- src/views/GoodsList.vue
+++ src/views/GoodsList.vue
@@ -11,16 +11,16 @@
             <span class="sortby">Sort by:</span>
             <a href="javascript:void(0)" class="default cur">Default</a>
             <a href="javascript:void(0)" class="price">Price <svg class="icon icon-arrow-short"><use xlink:href="#icon-arrow-short"></use></svg></a>
-            <a href="javascript:void(0)" class="filterby stopPop">Filter by</a>
+            <a href="javascript:void(0)" class="filterby stopPop" @click="showFilterPop">Filter by</a>
           </div>
           <div class="accessory-result">
             <!-- filter -->
-            <div class="filter stopPop" id="filter">
+            <div class="filter stopPop" id="filter" v-bind:class="{'filterby-show':filterBy}">
               <dl class="filter-price">
                 <dt>Price:</dt>
-                <dd><a href="javascript:void(0)">All</a></dd>
-                <dd>
-                  <a href="javascript:void(0)">0 - 100</a>
+                <dd><a href="javascript:void(0)" v-bind:class="{'cur':priceChecked=='all'}" @click="priceChecked='all'">All</a></dd>
+                <dd v-for="(price,index) in priceFilter" >
+                  <a href="javascript:void(0)" @click="setPriceFilter(index)" v-bind:class="{'cur':priceChecked==index}">{{price.startPrice}} - {{price.endPrice}}</a>
                 </dd>
               </dl>
             </div>
@@ -29,13 +29,13 @@
             <div class="accessory-list-wrap">
               <div class="accessory-list col-4">
                 <ul>
-                  <li>
+                  <li v-for="(item,index) in goodsList">
                     <div class="pic">
-                      <a href="#"><img src="/static/1.jpg" alt=""></a>
+                      <a href="#"><img v-lazy="'/static/'+item.productImg" alt=""></a>
                     </div>
                     <div class="main">
-                      <div class="name">XX</div>
-                      <div class="price">XX</div>
+                      <div class="name">{{item.productName}}</div>
+                      <div class="price">{{item.productPrice}}</div>
                       <div class="btn-area">
                         <a href="javascript:;" class="btn btn--m">加入购物车</a>
                       </div>
@@ -47,6 +47,7 @@
           </div>
         </div>
       </div>
+      <div class="md-overlay" v-show="overLayFlag" @click="closePop"></div>
       <nav-footer></nav-footer>
 
     </div>
@@ -63,7 +64,24 @@
         name: "GoodsList.vue",
       data(){
           return{
-            goodsList:[]
+            goodsList:[],
+            priceFilter:[
+              {
+                startPrice:'0.00',
+                endPrice:'500.00'
+              },
+              {
+                startPrice:'500.00',
+                endPrice:'1000.00'
+              },
+              {
+                startPrice:'1000.00',
+                endPrice:'2000.00'
+              }
+            ],
+            priceChecked:'all',
+            filterBy:false,
+            overLayFlag:false
           }
       },
       components:{
@@ -80,6 +98,18 @@
             var res = result.data;
             this.goodsList = res.result;
           })
+        },
+        showFilterPop(){
+          this.filterBy=true;
+          this.overLayFlag=true;
+        },
+        closePop(){
+          this.filterBy=false;
+          this.overLayFlag=false;
+        },
+        setPriceFilter(index){
+          this.priceChecked=index;
+          this.closePop();
         }
 
       }
